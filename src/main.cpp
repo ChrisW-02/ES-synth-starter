276d1d5c77cdb35a85a0e14630e0516e8f8fc331
Christina <chris.wbx@gmail.com>
2023-03-13 11:55:12 +0000

    working up to lab2 CAN part 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,211 +2,429 @@
 #include <U8g2lib.h>
 #include <STM32FreeRTOS.h>
 #include <iostream>
+#include <ES_CAN.h>
 
+// Constants
+const uint32_t interval = 100; // Display update interval
+volatile int32_t currentStepSize;
+volatile uint8_t keyArray[7];
+SemaphoreHandle_t keyArrayMutex;
+volatile int32_t knob3Rotation = 4;
+// volatile uint8_t TX_Message[8] = {0};
+uint8_t RX_Message[8] = {0};
+QueueHandle_t msgInQ;
 
-//Constants
-  const uint32_t interval = 100; //Display update interval
-  volatile int32_t currentStepSize;
-  volatile uint8_t keyArray[7];
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
-
-//Display driver object
+// Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, bitIdx & 0x01);
+  digitalWrite(RA1_PIN, bitIdx & 0x02);
+  digitalWrite(RA2_PIN, bitIdx & 0x04);
+  digitalWrite(OUT_PIN, value);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(REN_PIN, LOW);
 }
 
-uint8_t readCols(){
+uint8_t readCols()
+{
 
   int C0 = digitalRead(C0_PIN);
   int C1 = digitalRead(C1_PIN);
   int C2 = digitalRead(C2_PIN);
   int C3 = digitalRead(C3_PIN);
 
-  uint8_t res=0;
-  res |= (C0<<3);
-  res |= (C1<<2);
-  res |= (C2<<1);
+  uint8_t res = 0;
+  res |= (C0 << 3);
+  res |= (C1 << 2);
+  res |= (C2 << 1);
   res |= C3;
 
   return res;
 }
 
-int32_t fss(int frequency){
-  int32_t step = pow(2, 32)*frequency/22000;
+int32_t fss(int frequency)
+{
+  int32_t step = pow(2, 32) * frequency / 22000;
   return step;
 }
 
-void setRow(uint8_t rowIdx){
+void setRow(uint8_t rowIdx)
+{
   digitalWrite(REN_PIN, LOW);
-  digitalWrite(RA0_PIN, bitRead(rowIdx,0));
-  digitalWrite(RA1_PIN, bitRead(rowIdx,1));
-  digitalWrite(RA2_PIN, bitRead(rowIdx,2));
+  digitalWrite(RA0_PIN, bitRead(rowIdx, 0));
+  digitalWrite(RA1_PIN, bitRead(rowIdx, 1));
+  digitalWrite(RA2_PIN, bitRead(rowIdx, 2));
   digitalWrite(REN_PIN, HIGH);
 }
 
-int mapindex(volatile uint8_t keyarray[], int rowindex){
+int mapindex(uint8_t keypress, int rowindex)
+{
 
   int index;
- 
-  if(keyarray[rowindex] == 7){
-     index = 0+ rowindex*4;
+
+  if (keypress == 7)
+  {
+    index = 0 + rowindex * 4;
   }
-  else if(keyarray[rowindex] == 11){
-    index = 1+ rowindex*4;
+  else if (keypress == 11)
+  {
+    index = 1 + rowindex * 4;
   }
-  else if(keyarray[rowindex] == 13){
-    index = 2+ rowindex*4;
+  else if (keypress == 13)
+  {
+    index = 2 + rowindex * 4;
   }
-  else if(keyarray[rowindex] == 14){
-    index = 3+ rowindex*4;
+  else if (keypress == 14)
+  {
+    index = 3 + rowindex * 4;
   }
-  else{
+  else
+  {
     index = 12;
   }
   return index;
 }
 
+class Knob{
+  public:
+  uint8_t knob;
+  uint8_t preknob;
+  uint8_t knob_val;
+  bool increment;
+  int32_t rotation_var = 4;
 
-void sampleISR(){
+  int32_t detectknob3rot()
+  {
 
+    uint8_t knob = 0;
+    knob |= (bitRead(knob_val,2)<<1);
+    knob |= bitRead(knob_val,3);
+
+    if ((knob == 01 && preknob ==00) || (knob == 10 && preknob == 11))
+    {
+      rotation_var = rotation_var + 1;
+      increment = true;
+    }
+    else if ((knob == 00 && preknob == 01) || (knob == 11 && preknob == 10))
+    {
+      rotation_var = rotation_var - 1;
+      increment = false;
+    }
+    else if ((knob == 11 && preknob == 00) || (knob == 10 && preknob == 01) || (knob == 01 && preknob == 10) || (knob == 00 && preknob == 11))
+    {
+      if (increment == true){
+        rotation_var = rotation_var + 1;
+        increment = true;
+      }
+      else{
+        rotation_var = rotation_var - 1;
+        increment = false;
+      }
+    }
+    preknob = knob;
+    return rotation_var = constrain(rotation_var, 0, 8);
+  }
+};
+
+/*
+
+int32_t detectknob3rot(uint8_t &preknob3, uint8_t keypress, bool &flag)
+{
+  uint8_t knob3 = 0;
+  knob3 |= (bitRead(keypress, 2) << 1);
+  knob3 |= bitRead(keypress, 3);
+  if ((knob3 == 01 && preknob3 == 00) || (knob3 == 10 && preknob3 == 11))
+  {
+    knob3Rotation = knob3Rotation + 1;
+    flag = true;
+  }
+  else if ((knob3 == 00 && preknob3 == 01) || (knob3 == 11 && preknob3 == 10))
+  {
+    knob3Rotation = knob3Rotation - 1;
+    flag = false;
+  }
+  else if ((knob3 == 11 && preknob3 == 00) || (knob3 == 10 && preknob3 == 01) || (knob3 == 01 && preknob3 == 10) || (knob3 == 00 && preknob3 == 11))
+  {
+    if (flag == true)
+    {
+      knob3Rotation = knob3Rotation + 1;
+      flag = true;
+    }
+    else
+    {
+      knob3Rotation = knob3Rotation - 1;
+      flag = false;
+    }
+  }
+  preknob3 = knob3;
+  return knob3Rotation = constrain(knob3Rotation, 0, 8);
+}
+*/
+
+void sampleISR()
+{
   static uint32_t phaseAcc = 0;
   phaseAcc += currentStepSize;
   int32_t Vout = (phaseAcc >> 24) - 128;
+  Vout = Vout >> (8 - knob3Rotation);
   analogWrite(OUTR_PIN, Vout + 128);
-
 }
 
-
-void scanKeysTask(void* pvParameters){
-  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+void scanKeysTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 20 / portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  while(1){
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
-    const int32_t keys[13] = {fss(261.63),fss(277.18),fss(293.66),fss(311.13),fss(329.63),fss(349.23),fss(369.99),fss(392),fss(415.3),fss(440),fss(466.16),fss(493.88),0};
-    //volatile int32_t currentStepSize;
-    //uint8_t keyArray[7];
+  uint8_t prestate;
+  
+  Knob knob3;
+  knob3.preknob = prestate;
+  knob3.increment = false;
+
+  bool flag = false;
+  uint8_t TX_Message[8] = {0};
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    const int32_t keys[13] = {fss(261.63), fss(277.18), fss(293.66), fss(311.13), fss(329.63), fss(349.23), fss(369.99), fss(392), fss(415.3), fss(440), fss(466.16), fss(493.88), 0};
+    // volatile int32_t currentStepSize;
+    // uint8_t keyArray[7];
+
     int32_t stepSizes;
+    int32_t prestepSizes;
+    int32_t localknob3;
     bool press_key;
     press_key = false;
+    int preindex;
+    // uint8_t TX_Message[8] = {0};
 
-      for(int i=0; i<3; i++){
-        uint8_t rowindex = i;
-        setRow(rowindex);
-        delayMicroseconds(3);
-        keyArray[i] = readCols();
+    for (int i = 0; i < 4; i++)
+    {
+      uint8_t rowindex = i;
+      setRow(rowindex);
+      delayMicroseconds(3);
+      keyArray[i] = readCols();
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+      xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+      uint8_t keypresse = keyArray[i];
+      xSemaphoreGive(keyArrayMutex);
+      if (mapindex(keypresse, i) != 12 && press_key == false)
+      {
+        stepSizes = keys[mapindex(keypresse, i)];
+        press_key = true;
       }
-
-      for(int i=0;i<3;i++){
-
-        // if(mapindex(keyArray, i)==0){
-        //   u8g2.drawStr(2,10,"C");
-        // }
-        // else if(mapindex(keyArray, i)==1){
-        //   u8g2.drawStr(2,10,"C#");
-        // }
-        // else if(mapindex(keyArray, i)==2){
-        //   u8g2.drawStr(2,10,"D");
-        // }
-        // else if(mapindex(keyArray, i)==3){
-        //   u8g2.drawStr(2,10,"D#");
-        // }
-        // else if(mapindex(keyArray, i)==4){
-        //   u8g2.drawStr(2,10,"E");
-        // }
-        // else if(mapindex(keyArray, i)==5){
-        //   u8g2.drawStr(2,10,"F");
-        // }
-        // else if(mapindex(keyArray, i)==6){
-        //   u8g2.drawStr(2,10,"F#");
-        // }
-        // else if(mapindex(keyArray, i)==7){
-        //   u8g2.drawStr(2,10,"G");
-        // }
-        // else if(mapindex(keyArray, i)==8){
-        //   u8g2.drawStr(2,10,"G#");
-        // }
-        // else if(mapindex(keyArray, i)==9){
-        //   u8g2.drawStr(2,10,"A");
-        // }
-        // else if(mapindex(keyArray, i)==10){
-        //   u8g2.drawStr(2,10,"A#");
-        // }
-        // else if(mapindex(keyArray, i)==11){
-        //   u8g2.drawStr(2,10,"B");
-        // }
-        // else if(mapindex(keyArray, i)==12){
-        //   u8g2.drawStr(2,10," ");
-        //   stepSizes = stepSizes;
-        // }
-
-        // if(mapindex(keyArray, i)!=12){
-
-        //   u8g2.setCursor(2,20);
-        //   //u8g2.print(stepSizes[mapindex(keyArray, i)]);
-        //   u8g2.print(i);
-
-        //   stepSizes = keys[mapindex(keyArray, i)];
-
-        //   u8g2.setCursor(2,30);
-        //   u8g2.print(stepSizes);
-        // }
-
-        if(mapindex(keyArray,i) != 12 && press_key == false){
-          stepSizes = keys[mapindex(keyArray, i)];
-          press_key = true;
-        }
-        else if(mapindex(keyArray,i) == 12){
-          press_key = false;
-        }
-        if(keyArray[0]==15&&keyArray[1]==15&&keyArray[2]==15){
-          stepSizes=0;
-        }
-        __atomic_store_n(&currentStepSize, stepSizes, __ATOMIC_RELAXED);
+      else if (mapindex(keypresse, i) == 12)
+      {
+        press_key = false;
+      }
+      if (keyArray[0] == 15 && keyArray[1] == 15 && keyArray[2] == 15)
+      {
+        press_key = false;
+        stepSizes = 0;
+      }
+      if ((prestepSizes != stepSizes) && press_key == true)
+      {
+        TX_Message[0] = 'P';
+        TX_Message[1] = 4;
+        TX_Message[2] = mapindex(keypresse, i);
+        preindex = mapindex(keypresse, i);
+      }
+      if ((prestepSizes != stepSizes) && press_key == false)
+      {
+        TX_Message[0] = 'R';
+        TX_Message[1] = 4;
+        TX_Message[2] = preindex;
+      }
+      __atomic_store_n(&currentStepSize, stepSizes, __ATOMIC_RELAXED);
+      prestepSizes = stepSizes;
+      if (i == 3)
+      {
+        xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+        knob3.knob_val = keyArray[i];
+        //knob3.readknob();
+        localknob3 = knob3.detectknob3rot();  
+        prestate = knob3.preknob;    
+        //localknob3 = detectknob3rot(prestate, keypresse, flag);
+        xSemaphoreGive(keyArrayMutex);
+        __atomic_store_n(&knob3Rotation, localknob3, __ATOMIC_RELAXED);
       }
     }
+    CAN_TX(0x123, TX_Message);
+  }
 }
 
-void setup() {
+void displayUpdateTask(void *pvParameters)
+{
+
+  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    bool press_key;
+    press_key = false;
+    uint32_t ID;
+    //uint8_t RX_Message[8] = {0};
+
+    u8g2.clearBuffer();                 // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+
+    for (int i = 0; i < 3; i++)
+    {
+      xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+      uint8_t keypresse = keyArray[i];
+      u8g2.setCursor(2, 20);
+      u8g2.print(knob3Rotation, HEX);
+      xSemaphoreGive(keyArrayMutex);
+      if (mapindex(keypresse, i) == 0 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "C");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 1 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "C#");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 2 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "D");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 3 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "D#");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 4 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "E");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 5 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "F");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 6 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "F#");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 7 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "G");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 8 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "G#");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 9 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "A");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 10 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "A#");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 11 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, "B");
+        press_key = true;
+      }
+      else if (mapindex(keypresse, i) == 12 && press_key == false)
+      {
+        u8g2.drawStr(2, 30, " ");
+        press_key = false;
+      }
+
+      // __atomic_store_n(&currentStepSize, stepSizes, __ATOMIC_RELAXED);
+      // String display = mapkey(mapindex(keyArray,i));
+      // u8g2.print(display);
+      // u8g2.print(mapkey(mapindex(keyArray[i],i)));
+    }
+    while (CAN_CheckRXLevel())
+    u8g2.setCursor(66, 30);
+    u8g2.print((char)RX_Message[0]);
+    u8g2.print(RX_Message[1]);
+    u8g2.print(RX_Message[2]);
+    u8g2.sendBuffer();
+
+    // Toggle LED
+    digitalToggle(LED_BUILTIN);
+  }
+}
+  void CAN_RX_ISR(void)
+  {
+    uint8_t RX_Message_ISR[8];
+    uint32_t ID;
+    CAN_RX(ID, RX_Message_ISR);
+    xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+  }
+
+void decodeText(void *pvParameters)
+{
+  while (1)
+  {
+  xQueueReceive(msgInQ, RX_Message, portMAX_DELAY);
+  const int32_t keys[13] = {fss(261.63), fss(277.18), fss(293.66), fss(311.13), fss(329.63), fss(349.23), fss(369.99), fss(392), fss(415.3), fss(440), fss(466.16), fss(493.88), 0};
+  int32_t stepSizes;
+  if(RX_Message[0] == 'P'){
+    stepSizes = keys[RX_Message[2]];
+  }
+  else if(RX_Message[0] == 'R'){
+    stepSizes = 0;
+  }
+  
+  }
+}
+
+void setup()
+{
   // put your setup code here, to run once:
 
-  //Set pin directions
+  // Set pin directions
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
   pinMode(RA2_PIN, OUTPUT);
@@ -223,18 +441,29 @@ void setup() {
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+  // Initialise display
+  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
   delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
   u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  //Initialise UART
+  // Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
 
-  //creat a timer
+  // Create the mutex and assign its handle
+  keyArrayMutex = xSemaphoreCreateMutex();
+  // initialise queue handler
+  msgInQ = xQueueCreate(36, 8);
+
+  // initialise CAN
+  CAN_Init(true);
+  CAN_RegisterRX_ISR(CAN_RX_ISR);
+  setCANFilter(0x123, 0x7ff);
+  CAN_Start();
+
+  // creat a timer
   TIM_TypeDef *Instance = TIM1;
   HardwareTimer *sampleTimer = new HardwareTimer(Instance);
   sampleTimer->setOverflow(22000, HERTZ_FORMAT);
@@ -243,22 +472,32 @@ void setup() {
 
   TaskHandle_t scanKeysHandle = NULL;
   xTaskCreate(
-  scanKeysTask,		/* Function that implements the task */
-  "scanKeys",		/* Text name for the task */
-  64,      		/* Stack size in words, not bytes */
-  NULL,			/* Parameter passed into the task */
-  1,			/* Task priority */
-  &scanKeysHandle );  /* Pointer to store the task handle */
+      scanKeysTask,     /* Function that implements the task */
+      "scanKeys",       /* Text name for the task */
+      64,               /* Stack size in words, not bytes */
+      NULL,             /* Parameter passed into the task */
+      1,                /* Task priority */
+      &scanKeysHandle); /* Pointer to store the task handle */
+
+  TaskHandle_t displayHandle = NULL;
+  xTaskCreate(
+      displayUpdateTask,   /* Function that implements the task */
+      "displayupdatetask", /* Text name for the task */
+      256,                 /* Stack size in words, not bytes */
+      NULL,                /* Parameter passed into the task */
+      1,                   /* Task priority */
+      &displayHandle);     /* Pointer to store the task handle */
 
   vTaskStartScheduler();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
   // static uint32_t next = millis();
   // static uint32_t count = 0;
-  
+
   // if (millis() > next) {
   //   next += interval;
 
@@ -270,9 +509,9 @@ void loop() {
 
   //   scanKeysTask(k);
 
-  //   u8g2.sendBuffer();     
+  //   u8g2.sendBuffer();
   //   //currentStepSize = stepSizes[index];
-    
+
   //   //Toggle LED
   //   digitalToggle(LED_BUILTIN);
   // }
